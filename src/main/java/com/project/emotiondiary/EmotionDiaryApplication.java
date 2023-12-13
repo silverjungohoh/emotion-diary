b23fc5f7f845c50b1128a852202621e9537fa064
grace <grace994084@gmail.com>
2023-12-13 21:46:55 +0900

    initial commit

---
+++ src/main/java/com/project/emotiondiary/EmotionDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project.emotiondiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/emotiondiary/EmotionDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.emotiondiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
