ddfc7f1e55bf4f79aab251d720d6fc41e4e8d2bd
grace <grace994084@gmail.com>
2023-12-19 22:26:23 +0900

    feat : PasswordEncoder 및 SecurityFilterChain 빈 등록

---
+++ src/main/java/com/project/emotiondiary/global/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.project.emotiondiary.global.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(CsrfConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable);
+
+		http.sessionManagement(
+			sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		);
+
+		http.authorizeHttpRequests(
+			authorize ->
+				authorize
+					.requestMatchers("/api/members/sign-up", "/api/members/login", "/api/members/check/**")
+					.permitAll()
+					.requestMatchers(PathRequest.toH2Console())
+					.permitAll()
+					.anyRequest().authenticated()
+		);
+
+		http.headers(
+			headersConfigurer ->
+				headersConfigurer
+					.frameOptions(
+						HeadersConfigurer.FrameOptionsConfig::sameOrigin
+					)
+		);
+
+		return http.build();
+	}
+}
