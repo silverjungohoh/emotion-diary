0f6eca026f19d2d1f4fea22cadbdb85a63f97763
grace <grace994084@gmail.com>
2024-01-04 15:40:01 +0900

    fix : redis 설정 수정

--- src/main/java/com/project/emotiondiary/global/config/RedisConfig.java
+++ src/main/java/com/project/emotiondiary/global/config/RedisConfig.java
@@ -5,16 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories(
-	basePackages = "com.project.emotiondiary.global.auth.repository",
-	enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP
-)
+@EnableRedisRepositories(basePackages = "com.project.emotiondiary.global.auth.repository")
 public class RedisConfig {
 
 	@Value("${spring.data.redis.host}")
